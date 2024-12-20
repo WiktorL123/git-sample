66816c82de0c05570780a6229ad2db1cbfc8e9e8
Jakub Fahl <kubafahl@wp.pl>
2024-12-20 17:37:37 +0100

    lista przywrocona

--- addProduct.js
+++ addProduct.js
@@ -1,3 +1,20 @@
+const products = [
+    {
+        id: 1,
+        name: 'Chleb',
+        price: 100
+    },
+    {
+        id: 2,
+        name: 'China soup',
+        price: 20
+    },
+    {
+        id: 3,
+        name: 'Jogurt Waniliowy',
+        price: 3.50
+    }
+]
 
 
 function addProduct(productId, productName, productPrice) {
