17a47a3f834a1cdaae377c3d0620826c5175f59f
Jakub Fahl <kubafahl@wp.pl>
2024-12-20 17:26:49 +0100

    usunieto liste produktow

--- addProduct.js
+++ addProduct.js
@@ -1,20 +1,3 @@
-const products = [
-    {
-        id: 1,
-        name: 'Chleb',
-        price: 100
-    },
-    {
-        id: 2,
-        name: 'China soup',
-        price: 20
-    },
-    {
-        id: 3,
-        name: 'Jogurt Waniliowy',
-        price: 3.50
-    }
-]
 
 
 function addProduct(productId, productName, productPrice) {
